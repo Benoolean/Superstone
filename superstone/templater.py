c635bd97e2444494b4a6192ba154116e6a0ea071
Ben Xiao <benxiao778@gmail.com>
2018-07-24 11:02:36 -0400

    Fixed the redirection and session of admin login

--- superstone/templater.py
+++ superstone/templater.py
@@ -8,9 +8,14 @@ from werkzeug.datastructures import ImmutableDict
 from werkzeug.utils import secure_filename
 from bson import json_util
 from pymongo import MongoClient
-import os, json, pymongo, gridfs
+import os
+import json
+import pymongo
+import gridfs
 
-import db_stone, db_user, main
+import db_stone
+import db_user
+import main
 
 client = MongoClient('localhost', 27017)
 database = client.superstone
@@ -18,9 +23,12 @@ database = client.superstone
 templater = Blueprint('templater', __name__)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 class new_stone_form(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -29,32 +37,36 @@ class new_stone_form(FlaskForm):
     detail_description = StringField('Description', validators=[DataRequired()])
     photo = FileField('Photo', validators=[FileRequired()])
 
+
 class new_login_form(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = StringField('Password', validators=[DataRequired()])
+
 
 '''
 Template Render
 '''
 
+
 @templater.route('/')
 def main_page():
     return render_template('index.html')
 
+
 @templater.route('/admin/login', methods=['POST', 'GET'])
 def admin_login():
     if 'username' in session:
-        return redirect('http://www.google.com', code=200)
-        
+        return redirect('/admin/insert_stone', code=302)
+
     form = new_login_form()
     if request.method == 'POST':
         if form.validate():
             query_check = {
-                'username' : request.form['username'],
-                'password' : request.form['password']
+                'username': request.form['username'],
+                'password': request.form['password']
             }
 
-            result = db_user.find_one({'username' : query_check['username']})
+            result = db_user.find_one({'username': query_check['username']})
             if result == None:
                 return render_template('login.html', form=form, error='Username or password is invalid')
 
@@ -62,6 +74,8 @@ def admin_login():
                 print 'Password is correct'
                 session.permanent = True
                 session['username'] = query_check['username']
+                return redirect('/admin/insert_stone', code=302)
+
             else:
                 print 'Wrong password'
                 return render_template('login.html', form=form, error='Username or password is invalid')
@@ -69,18 +83,22 @@ def admin_login():
             print 'All the form fields are required.'
 
     return render_template('login.html', form=form)
+    
 
 @templater.route('/admin/insert_stone', methods=['POST', 'GET'])
 def contact_page():
+    if 'username' not in session:
+        return redirect('/admin/login', code=302)
+
     form = new_stone_form()
     if request.method == 'POST':
         if form.validate():
             query_insert = {
-                'name' : request.form['name'],
-                'stoneid' : db_stone.get_uuid(),
-                'sub_description' : request.form['sub_description'],
-                'price' : request.form['price'],
-                'detail_description' : request.form['detail_description'],
+                'name': request.form['name'],
+                'stoneid': db_stone.get_uuid(),
+                'sub_description': request.form['sub_description'],
+                'price': request.form['price'],
+                'detail_description': request.form['detail_description'],
             }
 
             img_count = 0
@@ -89,7 +107,8 @@ def contact_page():
                 print photo
                 filename = secure_filename(photo.filename)
                 datafile = photo.read()
-                db_stone.insert_image(datafile, filename , query_insert['stoneid'], img_count)
+                db_stone.insert_image(datafile, filename,
+                                      query_insert['stoneid'], img_count)
 
             query_insert['image_count'] = img_count
             db_stone.insert_one(query_insert)
@@ -106,9 +125,11 @@ def contact_page():
 
     return render_template('insert_stone.html', form=form)
 
+
 @templater.route('/products')
 def product_page():
     return render_template('projects.html')
+
 
 '''
 End of Template Render
@@ -126,14 +147,15 @@ End of POST Method
 GET Method
 '''
 
+
 @templater.route('/stone/detail/<stoneid>', methods=['GET'])
 def stone_details(stoneid):
-    query_return = db_stone.find_one({'stoneid' : str(stoneid)})
+    query_return = db_stone.find_one({'stoneid': str(stoneid)})
     if query_return == None:
         return redirect('http://www.google.com', code=302)
 
     img_urls = []
-    for image_count in range(1,query_return['image_count'] + 1):
+    for image_count in range(1, query_return['image_count'] + 1):
         url = '/stone/photo/static/view/' + stoneid + '/' + str(image_count)
         img_urls.append(str(url))
 
@@ -142,26 +164,29 @@ def stone_details(stoneid):
 
     return render_template('stone-detail.html', json=query_return, img_urls=img_urls)
 
+
 @templater.route('/stone/list', methods=['GET'])
 def stone_list():
-    query_return = db_stone.find_many({}) #returns everything
+    query_return = db_stone.find_many({})  # returns everything
     if query_return == None:
         return redirect("http://www.google.com", code=302)
 
     return render_template('stone-list.html', json=query_return)
 
+
 @templater.route('/stone/photo/static/view/<linkid>/<count>', methods=['GET'])
 def stone_photo(linkid, count):
     fs = gridfs.GridFS(database)
-    
-    query_return = db_stone.find_one_image({'linkid': linkid, 'img_count': int(count)})
+
+    query_return = db_stone.find_one_image(
+        {'linkid': linkid, 'img_count': int(count)})
     if query_return == None:
         return redirect('http://www.google.com', code=302)
 
     print query_return
     return Response(query_return, mimetype='image/jpeg')
 
+
 '''
 End of GET Method
 '''
-
